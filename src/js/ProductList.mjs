bc883de5c1f6f53bdad833490bfde6df21b439a8
Brendan Cluff <cluffbrendan04@gmail.com>
2026-03-17 15:08:11 -0700

    W02 Individual Activity: Dynamic Product List

---
+++ src/js/ProductList.mjs
@@ -0,0 +1,38 @@
+import { renderListWithTemplate } from "./utils.mjs";
+
+function productCardTemplate(product) {
+  return `
+    <li class="product-card">
+      <a href="/product_pages/index.html?product=${product.Id}">
+        <img src="${product.Image}" alt="Image of ${product.Name}">
+        <h3 class="card__brand">${product.Brand.Name}</h3>
+        <h2 class="card__name">${product.Name}</h2>
+        <p class="product-card__price">$${product.FinalPrice}</p>
+      </a>
+    </li>
+  `;
+}
+
+export default class ProductList {
+  constructor(category, dataSource, listElement) {
+    this.category = category;
+    this.dataSource = dataSource;
+    this.listElement = listElement;
+  }
+
+  async init() {
+    const products = await this.dataSource.getData();
+
+    const supportedProducts = ["880RT", "985RF", "344YJ", "344GT"];
+
+    const filteredProducts = products.filter((product) =>
+      supportedProducts.includes(product.Id)
+    );
+
+    this.renderList(filteredProducts);
+  }
+
+  renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list, "afterbegin", true);
+  }
+}
